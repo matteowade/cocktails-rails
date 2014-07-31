56119f576095c479ea00bb5ad8a98dca23cd3593
Matt Wade <mwade@rabbleandrouser.com>
2014-07-30 21:55:27 -0600

    tutorial code

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -1,2 +1,8 @@
 class Quantity < ActiveRecord::Base
+	attr_accessible :amount, :ingredient, :ingredient_attributes
+ 
+  	belongs_to :recipe
+  	belongs_to :ingredient
+ 
+  	accepts_nested_attributes_for :ingredient, :reject_if => :all_blank
 end
